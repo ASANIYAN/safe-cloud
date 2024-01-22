0f2030d6b1ac289c0bc7fd8b227f54c977a124d9
ASANIYAN <ayomidepaulasaniyan@gmail.com>
2024-01-22 11:22:07 +0100

    changed all functions to ES6 syntax

--- src/components/Context/AuthContext.js
+++ src/components/Context/AuthContext.js
@@ -1,123 +1,131 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { userDataRef } from '../Firebase/firebase';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup ,GoogleAuthProvider, sendPasswordResetEmail } from "firebase/auth";
-import { setDoc, doc } from 'firebase/firestore';
-
-
+import React, { useContext, useEffect, useState } from "react";
+import { userDataRef } from "../Firebase/firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
 
 const AuthContext = React.createContext();
 
-export function useAuth() {
-    return useContext(AuthContext)
-}
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
-export function AuthProvider(props) {
-    const [userData, setUserData] = useState({
-        firstName:"",
-        lastName:"",
-        email:"",
+export const AuthProvider = (props) => {
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const [currentUser, setCurrentUser] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [width, setWidth] = useState("");
+
+  const [folderName, setFolderName] = useState("");
+  const [fileName, setFileName] = useState("");
+
+  const [id, setId] = useState("");
+
+  //show delete folder modal
+  const [showDeleteFolderModal, setShowDeleteFolderModal] = useState(false);
+  //show delete file modal
+  const [showDeleteFileModal, setShowDeleteFileModal] = useState(false);
+  // for storing changes in searchBar value
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
+  const addDocument = (data, id) => {
+    return setDoc(doc(userDataRef, id), data);
+  };
+
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    addDocument(
+      {
+        displayName: result.user.displayName,
+        email: result.user.email,
+      },
+      result.user.uid
+    );
+    return result;
+  };
+
+  const logInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    return result;
+  };
+
+  const signup = async (email, password) => {
+    const result = await createUserWithEmailAndPassword(auth, email, password);
+    addDocument(
+      {
+        displayName: userData.firstName + " " + userData.lastName,
+        email: userData.email,
+      },
+      result.user.uid
+    );
+    return result;
+  };
+
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logout = () => {
+    return auth.signOut();
+  };
+
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
     });
-    
-    const [currentUser, setCurrentUser] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [width, setWidth] = useState("");
 
-    const [folderName, setFolderName] = useState("");
-    const [fileName, setFileName] = useState("");
+    return unsubscribe;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const [id, setId] = useState("");
-    
-    //show delete folder modal
-    const [showDeleteFolderModal, setShowDeleteFolderModal] = useState(false);
-    //show delete file modal
-    const [ showDeleteFileModal, setShowDeleteFileModal ] = useState(false);
-    // for storing changes in searchBar value
-    const [searchTerm, setSearchTerm] = useState("");
-    
-
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
-
-    function addDocument(data, id) {
-        return setDoc(doc(userDataRef, id), data);
-    }
-
-    function signInWithGoogle() {
-        return signInWithPopup(auth, provider).then((result) => {
-            addDocument({
-                displayName: result.user.displayName,
-                email: result.user.email
-            }, result.user.uid);
-        });
-    }
-
-    function logInWithGoogle() {
-        return signInWithPopup(auth, provider).then((result) => {
-        });
-    }
-
-    function signup(email, password) {
-        return createUserWithEmailAndPassword(auth, email, password).then((result) => {
-            addDocument({
-                displayName:  userData.firstName + " " + userData.lastName,
-                email: userData.email
-            }, result.user.uid);
-        });
-    }
-
-    function login(email, password) {
-        return signInWithEmailAndPassword(auth, email, password);
-    }
-
-    function logout() {
-        return auth.signOut();
-    }
-
-    function resetPassword(email) {
-        return sendPasswordResetEmail(auth, email);
-    }
-
-    useEffect(() =>{
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user);
-            setLoading(false);
-        })
-
-        return unsubscribe;
-    }, []);
-
-
-    const value = {
-        id,
-        setId,
-        fileName,
-        setFileName,
-        folderName,
-        setFolderName,
-        showDeleteFileModal,
-        setShowDeleteFileModal,
-        showDeleteFolderModal,
-        setShowDeleteFolderModal,
-        searchTerm,
-        setSearchTerm,
-        userData,
-        setUserData,
-        width,
-        setWidth,
-        currentUser,
-        addDocument,
-        signInWithGoogle,
-        logInWithGoogle,
-        login,
-        signup,
-        logout,
-        resetPassword
-    }
-    return (
-        <AuthContext.Provider value={value}>
-            {
-                !loading && props.children
-            }
-        </AuthContext.Provider>
-    )
-}
+  const value = {
+    id,
+    setId,
+    fileName,
+    setFileName,
+    folderName,
+    setFolderName,
+    showDeleteFileModal,
+    setShowDeleteFileModal,
+    showDeleteFolderModal,
+    setShowDeleteFolderModal,
+    searchTerm,
+    setSearchTerm,
+    userData,
+    setUserData,
+    width,
+    setWidth,
+    currentUser,
+    addDocument,
+    signInWithGoogle,
+    logInWithGoogle,
+    login,
+    signup,
+    logout,
+    resetPassword,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && props.children}
+    </AuthContext.Provider>
+  );
+};
